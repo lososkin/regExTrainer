625732544ef4390f4f49900831a8fb6708007301
Pavel Zolotov <yalososka@gmail.com>
2021-07-16 19:28:40 +0300

    fix bug with not valid regex

--- tasks/services.py
+++ tasks/services.py
@@ -4,6 +4,11 @@ def check_solution(task,solution):
     is_correct = True
     tests = models.Test.objects.filter(task=task)
     for test in tests:
-        if test.key!=bool(re.fullmatch(solution,test.test)):
+        try:
+            if test.key!=bool(re.fullmatch(solution,test.test)):
+                is_correct = False
+                break
+        except:
             is_correct = False
+            break
     return is_correct
